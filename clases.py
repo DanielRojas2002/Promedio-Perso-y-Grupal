a09cfd9bfc75a92c2e2c22dc758e9e3e4edc8ed6
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-07-13 17:22:19 -0500

    Agregue las clases que voy a usar y los metodos

---
+++ clases.py
@@ -0,0 +1,19 @@
+class Alumno:
+    def __init__ (self,calf1,nombre):
+        self.__calf1=calf1
+        self.__nombre=nombre
+        
+    def Promedio (self):
+        lista.append(self.__calf1)
+        if materias==contador:
+            suma=sum(lista)
+            promedioPersonal=suma/materias
+            lista2.append(promedioPersonal)
+            print("*"*50)
+            print(f"Promedio del Alumno {self.__nombre} :",{promedioPersonal})
+            print("*"*50)
+            
+class Final(Alumno):
+    def final(Alumno):
+        suma2=sum(lista2)
+        return(suma2)
